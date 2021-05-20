f6bd429c96381879b2a505e0430dacd70a7b79f2
hdeumpty <63592599+hdeumpty@users.noreply.github.com>
2021-05-20 22:58:35 +0200

    style page de login

--- login.php
+++ login.php
@@ -10,24 +10,40 @@
 <html>
   <?php require_once __DIR__ . '/head.php'; ?>
   <body>
-    <div id="container">
-      <!-- zone de connexion -->
-      <form action="mdp.php" method="POST">
-        <h1>Connexion</h1>
-          <label><b>Nom d'utilisateur</b></label>
-          <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-            <label><b>Mot de passe</b></label>
-          <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-          <input type="submit" id='submit' value='LOGIN' >
-          <?php
-            if(isset($_GET['erreur']))
-            {
-              $err = $_GET['erreur'];
-              if($err==1 || $err==2){echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";}
-              //if($err==2){echo "<p style='color:red'>Mot de passe incorrect</p>";}
-            }
-          ?>
-      </form>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <div class="card">
+            <div class="card-header text-center">
+              <img src="./images/hd_logo_dive_2_bleu.svg" alt="navbar brand" class="navbar-brand">
+            </div>
+            <div class="card-body">
+              <form action="mdp.php" method="POST">
+                <h1>Connexion</h1>
+                <?php
+                if(isset($_GET['erreur']))
+                {
+                  $err = $_GET['erreur'];
+                  if($err==1 || $err==2){echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";}
+                  //if($err==2){echo "<p style='color:red'>Mot de passe incorrect</p>";}
+                }
+                ?>
+                <div class="form-group">
+                  <label>Nom d'utilisateur</label>
+                  <input type="text" class="form-control" placeholder="nom d'utilisateur" name="username" required>
+                </div>
+                <div class="form-group">
+                  <label>Mot de passe</label>
+                  <input type="password" class="form-control" placeholder="mot de passe" name="password" required>
+                </div>
+                <div class="form-group">
+                  <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">Sign in</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </body>
 </html>
