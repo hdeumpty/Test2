9e54b9e28056cd0ad48852d71ebbce2981cb891c
hdeumpty <63592599+hdeumpty@users.noreply.github.com>
2021-05-21 11:41:02 +0200

    amélioration login

--- login.php
+++ login.php
@@ -12,14 +12,15 @@
   <body>
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-6">
-          <div class="card">
-            <div class="card-header text-center">
-              <img src="./images/hd_logo_dive_2_bleu.svg" alt="navbar brand" class="navbar-brand">
+        <!--div class="col-5"-->
+          <div class="card"  style="width: 400px;">
+            <div class="card-header text-center" style="background-color: #1079cf;border-top-left-radius: 5px;border-top-right-radius: 5px;">
+              <img src="./images/hd_logo_dive_239x60_gris.svg">
             </div>
             <div class="card-body">
               <form action="mdp.php" method="POST">
-                <h1>Connexion</h1>
+
+                <h3>Entrez vos identifiants pour démarrer une session</h3>
                 <?php
                 if(isset($_GET['erreur']))
                 {
@@ -37,12 +38,12 @@
                   <input type="password" class="form-control" placeholder="mot de passe" name="password" required>
                 </div>
                 <div class="form-group">
-                  <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">Sign in</button>
+                  <button type="submit" class="btn" style="background:#1079cf;color: #fff;">Connexion</button>
                 </div>
               </form>
             </div>
           </div>
-        </div>
+        <!--/div-->
       </div>
     </div>
   </body>
